3e210e91b3d89900d5f6a17c4dafc090801f6913
dakkani <135724674+dakkani@users.noreply.github.com>
2025-05-01 15:10:29 +0530

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,6 +1,8 @@
 // vars/dockerPush.groovy
-def call(String imageName, String registry = '') {
-    echo "Pushing Docker image: ${imageName} to ${registry}"
-    def fullImageName = registry ? "${registry}/${imageName}" : imageName
-    sh "docker push ${fullImageName}"
+def call() {
+    def imageName = 'omer2k1/hiring-app:latest'
+    echo "Pushing Docker image: ${imageName}"
+    withCredentials([usernamePassword(credentialsId: 'docker-id', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
+        sh "docker push ${imageName}"
+    }
 }
