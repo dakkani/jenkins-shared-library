73106a2cf194e89df9e2dae33c99b49a36416a5a
dakkani <135724674+dakkani@users.noreply.github.com>
2025-05-01 13:00:12 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,5 @@
 // vars/dockerBuild.groovy
 def call(String imageName, String buildArgs = '') {
     echo "Building Docker image: ${imageName}"
-    def args = buildArgs ? "--build-arg ${buildArgs}" : ''
-    sh "docker build ${args} -t ${imageName} ."
+    sh "docker build ${buildArgs ? "--build-arg ${buildArgs}" : ''} -t ${imageName} ."
 }
